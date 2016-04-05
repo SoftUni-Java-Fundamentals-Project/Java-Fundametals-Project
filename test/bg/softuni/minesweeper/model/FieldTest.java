c813a6ae004e280b2e41c2e35cbedb11bb16ee76
Evgeniya Lyubomirova <evgeniyalyubomirova1@gmail.com>
2016-04-05 21:31:11 +0300

    Added unit tests

---
+++ test/bg/softuni/minesweeper/model/FieldTest.java
@@ -0,0 +1,29 @@
+package bg.softuni.minesweeper.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FieldTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getRows() throws Exception {
+
+    }
+
+    @Test
+    public void getColumns() throws Exception {
+
+    }
+
+    @Test
+    public void getMinesCount() throws Exception {
+
+    }
+}
